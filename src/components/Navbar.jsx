cdcaed3394211d7e903049118da5b046db49eb62
saahiyo <linkg518@example.com>
2024-10-28 18:21:04 +0530

    modified:   index.html 	new file:   public/favicon.ico 	deleted:    public/vite.svg 	modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="w-full h-[10vh] flex justify-between text-white items-center px-10 border-b-2 border-b-orange-950">
       <div className="navLeft text-orange-500 text-2xl flex gap-5">
         <h1>Naruto universe</h1>
-        <img src="./public/weapon.png" alt="logo" className="w-8 h-8 " />
+        <img src="./weapon.png" alt="logo" className="w-8 h-8 " />
       </div>
       <div className="navRight">
         <ul className="flex gap-5">
